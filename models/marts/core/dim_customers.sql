21892bcdd58914bff27a44d3bbc1c47b06f3815b
allenyeung24@gmail.com <allenyeung24@gmail.com>
2021-10-18 20:57:28 +0000

    test

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,7 +1,3 @@
-{{ config(
-    materialized="table"
-)}}
-
 with customers as (
     select * from {{ ref('stg_customers') }}
 ),
@@ -14,7 +10,6 @@ customer_orders as (
 
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
@@ -25,6 +20,13 @@ customer_orders as (
 
 ),
 
+customer_orders_spend as (
+    select 
+        customer_id,
+        SUM(amount) as lifetime_value
+    from {{ ref('fct_orders') }}
+    group by customer_id
+),
 
 final as (
 
@@ -34,11 +36,13 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders_spend.lifetime_value
 
     from customers
 
     left join customer_orders using (customer_id)
+    left join customer_orders_spend using (customer_id)
 
 )
 
---
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -0,0 +1,11 @@
+with payments as (
+    select 
+        orderid as order_id,
+        paymentmethod as payment_method,
+        status,
+        amount/100 as amount,
+        created as created_at
+    from raw.stripe.payment
+)
+
+select * from payments
